b817402668019641243faf6fb105a662fd1aeed3
Yael Solan <yaelsolan5@gmail.com>
2022-10-09 20:35:01 +0300

    initial commit

---
+++ Pyshop/products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import  path
+from . import views
+
+#
+# /products/1/detail
+# /products/new
+
+urlpatterns = [
+    path('', views.index),
+    path('new', views.new)
+]
---
+++ Pyshop/products/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New Products')
+
