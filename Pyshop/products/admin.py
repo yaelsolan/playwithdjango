c29304745a1e6131e3ef14cb5b6d459eb3fe7e9f
Yael Solan <yaelsolan5@gmail.com>
2022-10-20 16:57:50 +0300

    add more Modules in admin

--- Pyshop/products/admin.py
+++ Pyshop/products/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from.models import Product
+from.models import Product, Offer
 
-admin.site.register(Product)
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+
+admin.site.register(Offer, OfferAdmin)
+admin.site.register(Product, ProductAdmin)
--- Pyshop/products/views.py
+++ Pyshop/products/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 
 def index(request):
-    return HttpResponse('Hello World')
-
+    products = Product.objects.all()
+    return render(request, 'index.html',
+                  {'products': products})
 
 def new(request):
     return HttpResponse('New Products')
